6885205eb9b5c0ed78c0ff6d8eb8475d0a354355
shankkow <shashankssk95@gmail.com>
2024-10-21 14:02:21 +0530

    final project commits

--- app/src/main/java/com/example/teachmintgithub/presentation/MainActivity.kt
+++ app/src/main/java/com/example/teachmintgithub/presentation/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.teachmintgithub.presentation
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.example.teachmintgithub.R
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.assignmenttrial1.presentation.ViewModel.ContributorViewModel
+import com.example.assignmenttrial1.presentation.adapter.ContributorAdapter
+import com.example.teachmintgithub.presentation.ViewModel.ContributorViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contributorRecyclerView: RecyclerView
+    private lateinit var contributorAdapter: ContributorAdapter
+    private val contributorViewModel: ContributorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contributorRecyclerView = findViewById(R.id.recycler_view_contributors)
+        contributorRecyclerView.layoutManager = LinearLayoutManager(this)
+        contributorAdapter = ContributorAdapter(this, mutableListOf())
+        contributorRecyclerView.adapter = contributorAdapter
+
+        contributorViewModel.contributors.observe(this) { contributors ->
+            contributorAdapter.updateData(contributors)
+        }
+
+        contributorViewModel.fetchContributors("owncloud", "android", 0) // Initial fetch
     }
 }
